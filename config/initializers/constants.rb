6d4662a60b45b50483a9a4dc3be25123077bd91b
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2016-11-30 08:49:42 +0900

    fix split method in constants

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -2,7 +2,7 @@ module Constants
   PREFIX_KEY ||= 'tmb'
   EVENT_TYPE_MESSAGE ||= 'message'
   COMMANDS ||= {PING: 'ping', SET_CLEANING_DATE: 'set-cleaning-date', GET_CLEANING_DATE: 'get-cleaning-date', SET_DAIJIN: 'set-daijin',GET_DAIJIN: 'get-daijin', GET_ACCOUNT: 'get-account',HAT: 'hat' , HELP: 'help'}
-  MEMBER_LIST ||= (ENV['MEMBER_LIST']||'Tom,John').split
-  ELIGIBLE_SOURCE ||= (ENV['ELIGIBLE_SOURCE']||'111,222').split
+  MEMBER_LIST ||= (ENV['MEMBER_LIST']||'Tom,John').split(',')
+  ELIGIBLE_SOURCE ||= (ENV['ELIGIBLE_SOURCE']||'111,222').split(',')
   ACCOUNT ||= ENV['ACCOUNT']
 end
